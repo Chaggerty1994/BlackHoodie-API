4dad0d7ce087b4580437bd16a494d697c6d91dce
Christian Haggerty <captivationstation@gmail.com>
2022-06-08 10:38:09 -0500

    models made

---
+++ app_api/models/product.py
@@ -0,0 +1,24 @@
+from operator import truediv
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField(validators=[MinValueValidator(0.00), MaxValueValidator(1000.00)])
+    category = models.ForeignKey(
+        "category", on_delete=models.CASCADE, related_name='products')
+    image_path = models.ImageField(upload_to='products', height_field=None, 
+                                    width_field=None, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super().save(*args, **kwargs)
+
+    @property
+    def number_purchased(self):
+        """Returns the number of times product shows up on completed orders
+        """
+        return self.orders.exclude(payment_type=None).count()
+
+    def __str__(self):
+        return self.title
---
+++ app_api/models/user_payment.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserPayment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='payment_types')
+    card_number = models.CharField(max_length=16)
+    exp_date = models.CharField(max_length=5)
+    cvv = models.IntegerField(max_length=3)
+
+    @property
+    def obscured_num(self):
+        """Obscure the account number
+
+        Returns:
+            string: ex. **********1234
+        """
+        return '*'*(len(self.card_number) - 4)+self.card_number[-4:]
